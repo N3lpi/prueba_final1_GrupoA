9f8d02e667f01bc7e59495f75db6dff02d59de02
nelzone89 <e1723583348@uisrael.edu.ec>
2023-09-30 00:14:49 -0500

    Primera subida

---
+++ mongodb.py
@@ -0,0 +1,22 @@
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # take environment variables from .env.
+
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+
+uri = f"mongodb+srv://tratamientodatos2:tr4t4m13nt0d4t0s2@cluster0.fo30jxs.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+client.get_database('final2').get_collection('repositorio').insert_one(document={"indicativo": "5530E", "n_tor_n": "30", "q_max_n": "29"})
